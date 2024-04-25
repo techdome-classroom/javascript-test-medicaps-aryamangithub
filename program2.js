f457bae6b03e6ab715f116a6d616526648ed4ce6
Aryaman Chandrawat <singharyaman357@gmail.com>
2024-04-25 13:32:02 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -4,6 +4,7 @@ function smallestMissingPositiveInteger(nums) {
   while(i < nums.length){
     if(arr[i] > 0 && arr[i] <= arr.length && arr[arr[i] - 1] !== arr[i]) {
       [arr[arr[i] - 1], arr[i]] = [arr[i], arr[arr[i] - 1]]
+      
     }
   }
 }
