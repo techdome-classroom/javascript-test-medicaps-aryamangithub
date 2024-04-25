1cc0ebc9fc834b97958303d58000002fa8297551
Aryaman Chandrawat <singharyaman357@gmail.com>
2024-04-25 13:22:42 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,7 +1,10 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
     const charIndexMap = {}
-    
+    let maxLength = 0
+    let windowStart = 0
+
+    for(let windowEnd = )
 }
 
 module.exports = { longestSubstring };
