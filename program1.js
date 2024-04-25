40155bd38839bab1f9e9ce9b4eb99c5426945670
Aryaman Chandrawat <singharyaman357@gmail.com>
2024-04-25 13:24:11 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -6,7 +6,7 @@ function longestSubstring(s) {
 
     for(let windowEnd = 0; windowEnd < s.length; windowEnd++) {
         const rightChar = s[windowEnd]
-        if(charIndexMap[rightChar])
+        if(charIndexMap[rightChar]!==undefined) {
     }
 }
 
