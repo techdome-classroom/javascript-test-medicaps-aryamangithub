cb106322c79724f73127c051ca849446642da4b2
Aryaman Chandrawat <singharyaman357@gmail.com>
2024-04-25 13:30:58 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,9 @@
 function smallestMissingPositiveInteger(nums) {
   // Implement the function smallest_missing_positive_integer
   let i=0
-  
+  while(i < nums.length){
+    if(arr[i] > 0 && arr[i] <= arr.length && arr[arr[i] - 1] !== arr[i])
+  }
 }
 
 module.exports = smallestMissingPositiveInteger;
